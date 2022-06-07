5b1f3def578e3f703c8cc95571b6ffa25903684a
algo7 <11154774+algo7@users.noreply.github.com>
2022-06-07 17:24:29 +0200

    add err handler

--- utils/utils.go
+++ utils/utils.go
@@ -1,5 +1,16 @@
 package utils
 
-func errorHandler() {
+import (
+	"fmt"
+	"os"
+)
 
+// Custom error handler
+func errorHandler(err error) {
+	if err != nil {
+		fmt.Println(err)
+		fmt.Println("Press Any Key to Exit...")
+		fmt.Scanln()
+		os.Exit(0)
+	}
 }
