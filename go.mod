e6225446fcce0aa425de2c07466b04325b282580
algo7 <11154774+algo7@users.noreply.github.com>
2022-06-07 17:59:37 +0200

    add tail package

--- go.mod
+++ go.mod
@@ -1,3 +1,11 @@
 module tf2-rcon
 
 go 1.18
+
+require github.com/nxadm/tail v1.4.8
+
+require (
+	github.com/fsnotify/fsnotify v1.4.9 // indirect
+	golang.org/x/sys v0.0.0-20191005200804-aed5e4c7ecf9 // indirect
+	gopkg.in/tomb.v1 v1.0.0-20141024135613-dd632973f1e7 // indirect
+)
--- main.go
+++ main.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"fmt"
+	"strings"
 	"tf2-rcon/network"
 	"tf2-rcon/utils"
+
+	"github.com/nxadm/tail"
 )
 
 // Global variables
@@ -14,6 +18,60 @@ func main() {
 	rconHost := network.DetermineRconHost()
 	if rconHost == "Nothing" {
 		utils.ErrorHandler(utils.ErrMissingRconHost)
+	}
+	fmt.Printf("Rcon Host: %s\n", rconHost)
+
+	// Tail tf2 console log
+	t, err := tail.TailFile(
+		winTf2LogPath,
+		tail.Config{
+			MustExist: true,
+			Follow:    true,
+			Poll:      true,
+		})
+	utils.ErrorHandler(err)
+	// Loop through the text of each received line
+	for line := range t.Lines {
+
+		// Function 1
+		if strings.Contains(line.Text, "killed Algo7") &&
+			!strings.Contains(line.Text, "(crit)") {
+			// Send rcon command
+			rconExecute(conn, "say \"Algo7 Down\"")
+		}
+
+		// Function 2
+		// if strings.Contains(line.Text, "killed") &&
+		// 	strings.Contains(line.Text, "(crit)") &&
+		// 	!strings.Contains(line.Text, "Algo7") {
+		// 	killer := strings.Split(line.Text, "killed")
+		// 	victim := strings.TrimSpace(strings.Split(killer[1], "with")[0])
+		// 	fmt.Println(line.Text)
+		// 	fmt.Println(killer[0])
+		// 	fmt.Println(victim)
+		// 	fmt.Println("say" + " " + "\"" + killer[0] + "nice crit" + "\"")
+
+		// 	rconExecute(conn, "say"+" "+"\""+killer[0]+"nice crit"+"\"")
+
+		// }
+
+		// Function 3
+		if strings.Contains(line.Text, "killed") &&
+			strings.Contains(line.Text, "(crit)") &&
+			strings.Contains(line.Text, "Algo7") {
+			killer := strings.Split(line.Text, "killed")
+			victim := strings.TrimSpace(strings.Split(killer[1], "with")[0])
+			fmt.Println(line.Text)
+			fmt.Println(killer[0])
+			fmt.Println(victim)
+			fmt.Println("say" + " " + "\"" + killer[0] + "nice crit" + "\"")
+
+			rconExecute(conn, ("say" + " " + "\"" + killer[0] + "nice crit" + "\""))
+
+		}
+
+		fmt.Println(line.Text)
+
 	}
 
 }
