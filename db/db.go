616dcb4eb1c7c881515070f3ed1d518b320dd64d
algo7 <11154774+algo7@users.noreply.github.com>
2023-02-11 21:22:57 +0100

    comment out struct

--- db/db.go
+++ db/db.go
@@ -6,14 +6,15 @@ import (
 
 	"tf2-rcon/utils"
 
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-type Player struct {
-	steamID   string
-	encounter int32
-}
+// type Player struct {
+// 	steamID   string
+// 	encounter int32
+// }
 
 func DBConnect() *mongo.Client {
 	// Set client options
@@ -29,4 +30,18 @@ func DBConnect() *mongo.Client {
 	fmt.Println("Connected to MongoDB!")
 
 	return client
+}
+
+func DBAddPlayer(client *mongo.Client, playerID string) {
+	collection := client.Database("TF2").Collection("Players")
+
+	filter := bson.D{{Key: "SteamID", Value: playerID}}
+	update := bson.D{{Key: "$set", Value: bson.D{{Key: "Encounter", Value: 1}}}}
+	opts := options.Update().SetUpsert(true)
+	result, err := collection.UpdateOne(context.TODO(), filter, update, opts)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("Number of documents updated: %v\n", result.ModifiedCount)
+	fmt.Printf("Number of documents upserted: %v\n", result.UpsertedCount)
 }
