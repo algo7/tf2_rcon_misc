309ccbfe1573614b220fa6b9684c324a8d2a4e16
algo7 <11154774+algo7@users.noreply.github.com>
2023-03-08 23:37:39 +0100

    compliment api

---
+++ gpt/compliment.go
@@ -0,0 +1,42 @@
+package gpt
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"net/http"
+	"tf2-rcon/network"
+	"tf2-rcon/utils"
+	"time"
+)
+
+// GetCompliment returns a compliment for the given target
+func GetCompliment(target string) {
+	fmt.Println("Getting compliment for " + target)
+
+	// Send GET request to API
+	resp, err := http.Get("https://complimentr.com/api")
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+
+	// Parse response JSON
+	var data map[string]interface{}
+
+	err = json.NewDecoder(resp.Body).Decode(&data)
+	if err != nil {
+		utils.ErrorHandler(err)
+	}
+
+	// Extract insult from response data
+	compliment, ok := data["compliment"].(string)
+	if !ok {
+		utils.ErrorHandler(errors.New("Could not parse insult from response data"))
+	}
+
+	time.Sleep(1000 * time.Millisecond)
+
+	network.RconExecute("say \"" + compliment + "\"")
+	fmt.Println("Insult: " + compliment)
+}
