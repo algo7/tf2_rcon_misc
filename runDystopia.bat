67236b530c9ef283a2e35f22818942bc0124ec41
atomy <atomy@jackinpoint.net>
2023-03-16 03:11:29 +0100

    rm mongo

--- runDystopia.bat
+++ runDystopia.bat
@@ -1,6 +1,6 @@
 @echo off
 
-set "MONGODB_URI=mongodb+srv://mongo:yKhJM60c5xITh1yedN81@cluster0.4gjncyl.mongodb.net/?retryWrites=true&w=majority"
+set "MONGODB_URI=mongodb+srv://mongo:x@x.x.mongodb.net/?retryWrites=true&w=majority"
 set TF2_LOGPATH=C:\\Steam\\steamapps\\common\\Dystopia\\dystopia\\console.log
 set MONGODB_NAME=TeamFortress2
 set OPENAI_APIKEY=xxx
