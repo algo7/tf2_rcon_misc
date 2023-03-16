3d1386ad1efce25e675e50506f4e6e6ed7daaf49
atomy <atomy@jackinpoint.net>
2023-03-16 02:08:30 +0100

    added script for starting on dystopia

---
+++ runDystopia.bat
@@ -0,0 +1,9 @@
+@echo off
+
+set "MONGODB_URI=mongodb+srv://mongo:yKhJM60c5xITh1yedN81@cluster0.4gjncyl.mongodb.net/?retryWrites=true&w=majority"
+#set TF2_LOGPATH=D:\\Program Files (x86)\\Steam\\steamapps\\common\\Team Fortress 2\\tf\\console.log
+set TF2_LOGPATH=C:\\Steam\\steamapps\\common\\Dystopia\\dystopia\\console.log
+set MONGODB_NAME=TeamFortress2
+set OPENAI_APIKEY=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
+
+.\build\main-windows-amd64.exe
