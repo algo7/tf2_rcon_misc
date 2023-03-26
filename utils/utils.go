b83a0b0ff16d7324ccab795d5280ce817c0a8278
atomy <atomy@jackinpoint.net>
2023-03-26 01:04:52 +0100

    fixed array out of bounds

--- utils/utils.go
+++ utils/utils.go
@@ -244,8 +244,6 @@ func GetChatSayTF2(players []string, in string) (bool, string, string) {
 func GetChatSayDystopia(players []string, in string) (bool, string, string) {
 
 	for _, player := range players {
-		fmt.Println("in:", in[1:len(player)+1])
-		fmt.Println("in2:", in[len(player)+1:len(player)+2])
 
 		// check if we found a player saying that in our playerlist
 		if len(in) > len(player)+5 && in[1:len(player)+1] == player && in[len(player)+1:len(player)+2] == ":" {
