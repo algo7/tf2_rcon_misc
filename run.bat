42f3a118530765de4005c55bbb9357049028b0c7
atomy <atomy@jackinpoint.net>
2023-03-28 01:30:52 +0200

    added example set openai key

--- run.bat
+++ run.bat
@@ -4,5 +4,6 @@ set "MONGODB_URI=mongodb://localhost:27017"
 set TF2_LOGPATH=C:\\Program Files (x86)\\Steam\\steamapps\\common\\Team Fortress 2\\tf\\console.log
 set MONGODB_NAME=TF2
 set ENABLE_AUTOBALANCE_COMMENT=1
+set OPENAI_APIKEY=
 
 .\build\main-windows-amd64.exe
