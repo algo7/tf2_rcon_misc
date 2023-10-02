00d885bbb08e2674fdfd78d82a46cf625cda612a
algo7 <11154774+algo7@users.noreply.github.com>
2023-10-02 06:43:09 +0200

    remove deprecated code

--- commands/commands.go
+++ commands/commands.go
@@ -6,15 +6,6 @@ import (
 	"github.com/algo7/tf2_rcon_misc/network"
 )
 
-// otherUsersCommandMap is a map of functions for chat-commands that everyone (but you) is allowed to execute
-var otherUsersCommandMap = map[string]func(args string){
-	// Stuff follows the : are only function pointers not function calls
-	"!nice": func(args string) {
-		time.Sleep(1000 * time.Millisecond)
-		getCompliment(args)
-	},
-}
-
 // CommandExecuted is a function that executes a given command using the given arguments
 func CommandExecuted(command string, args string, callerName string, currentPlayer string) {
 
