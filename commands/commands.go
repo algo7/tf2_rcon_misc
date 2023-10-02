eaf5a17c647e54fb21d0e3883b13d230df660a75
algo7 <11154774+algo7@users.noreply.github.com>
2023-10-02 06:37:58 +0200

    diff commands for user itself and other players

--- commands/commands.go
+++ commands/commands.go
@@ -16,8 +16,23 @@ var otherUsersCommandMap = map[string]func(args string){
 }
 
 // CommandExecuted is a function that executes a given command using the given arguments
-func CommandExecuted(command string, args string) {
+func CommandExecuted(command string, args string, callerName string, currentPlayer string) {
 
+	// If the caller is the current player
+	if callerName == currentPlayer {
+		switch command {
+		case "test":
+			time.Sleep(1000 * time.Millisecond)
+			network.RconExecute("say \"Test command executed!. Value:" + args + "\"")
+		case "roast":
+			time.Sleep(1000 * time.Millisecond)
+			getInsult(args)
+		default:
+			return
+		}
+	}
+
+	// If the caller is not the current player
 	switch command {
 	case "test":
 		time.Sleep(1000 * time.Millisecond)
@@ -25,5 +40,8 @@ func CommandExecuted(command string, args string) {
 	case "roast":
 		time.Sleep(1000 * time.Millisecond)
 		getInsult(args)
+	default:
+		return
 	}
+
 }
